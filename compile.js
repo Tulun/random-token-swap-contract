23df85f6287bf4666aed1b615de8d05de5936e12
Jason Kiraly <jaykiraly@gmail.com>
2018-11-16 13:44:38 -0800

    basic token compiles tokay.

--- compile.js
+++ compile.js
@@ -7,33 +7,35 @@ const buildPath = path.resolve(__dirname, "build");
 // Remove build folder.
 fs.removeSync(buildPath);
 
-// const leaderboardPath = path.resolve(__dirname, "contracts", "leaderboard", "Leaderboard.sol");
-// const safeMathPath = path.resolve(__dirname, "contracts", "leaderboard", "SafeMath.sol");
-// const stringUtilsPath = path.resolve(__dirname, "contracts", "leaderboard", "StringUtils.sol");
-// const reentrancyGuardPath = path.resolve(__dirname, "contracts", "leaderboard", "ReentrancyGuard.sol");
+const tokenPath = path.resolve(__dirname, "contracts", "token", "BasicToken.sol");
+const approveAndCallFallBackPath = path.resolve(__dirname, "contracts", "token", "ApproveAndCallFallBack.sol");
+const erc20InterfacePath = path.resolve(__dirname, "contracts", "token", "ERC20Interface.sol");
+const ownedPath = path.resolve(__dirname, "contracts", "token", "Owned.sol");
+const safeMathPath = path.resolve(__dirname, "contracts", "token", "SafeMath.sol");
 
-// const input = {
-//   sources: {
-//     "Leaderboard.sol": fs.readFileSync(leaderboardPath, "utf8"),
-//     "SafeMath.sol": fs.readFileSync(safeMathPath, "utf8"),
-//     "StringUtils.sol": fs.readFileSync(stringUtilsPath, "utf8"),
-//     "ReentrancyGuard.sol": fs.readFileSync(reentrancyGuardPath, "utf8")
-//   }
-// }
+const input = {
+  sources: {
+    "BasicToken.sol": fs.readFileSync(tokenPath, "utf8"),
+    "ApproveAndCallFallBack.sol": fs.readFileSync(approveAndCallFallBackPath, "utf8"),
+    "ERC20Interface.sol": fs.readFileSync(erc20InterfacePath, "utf8"),
+    "Owned.sol": fs.readFileSync(ownedPath, "utf8"),
+    "SafeMath.sol": fs.readFileSync(safeMathPath, "utf8"),
+  }
+}
 
-// const output = solc.compile(input, 1);
-// // console.log('o', output, output.contracts);
-// const contracts = output.contracts;
-// fs.ensureDirSync(buildPath);
+const output = solc.compile(input, 1);
+console.log('o', output, output.contracts);
+const contracts = output.contracts;
+fs.ensureDirSync(buildPath);
 
-// for (let contract in contracts) {
-//   const filename = contract.split(".")[0];
-//   console.log(`interface for ${contract}: `, contracts[contract].interface)
-//   fs.outputJsonSync(
-//     path.resolve(buildPath, `${filename}.json`),
-//     contracts[contract]
-//   );
-// }
+for (let contract in contracts) {
+  const filename = contract.split(".")[0];
+  console.log(`interface for ${contract}: `, contracts[contract].interface)
+  fs.outputJsonSync(
+    path.resolve(buildPath, `${filename}.json`),
+    contracts[contract]
+  );
+}
 
 console.log('compile successful!');
 console.log('Your ABIs might need to be updated on any frontend apps.')
--- contracts/token/BasicToken.sol
+++ contracts/token/BasicToken.sol
@@ -1,113 +1,23 @@
-pragma solidity 0.4.24;
+pragma solidity ^0.4.24;
 
+import "./Owned.sol";
+import "./SafeMath.sol";
+import "./ApproveAndCallFallBack.sol";
+import "./ERC20Interface.sol";
 
 // Need this for testing
-
-// ----------------------------------------------------------------------------
-// 'FIXED' 'Example Fixed Supply Token' token contract
-//
-// Symbol      : FIXED
-// Name        : Example Fixed Supply Token
-// Total supply: 1,000,000.000000000000000000
-// Decimals    : 18
-//
-// Enjoy.
-//
-// (c) BokkyPooBah / Bok Consulting Pty Ltd 2018. The MIT Licence.
-// ----------------------------------------------------------------------------
-
-
-// ----------------------------------------------------------------------------
-// Safe maths
-// ----------------------------------------------------------------------------
-library SafeMath {
-    function add(uint a, uint b) internal pure returns (uint c) {
-        c = a + b;
-        require(c >= a);
-    }
-    function sub(uint a, uint b) internal pure returns (uint c) {
-        require(b <= a);
-        c = a - b;
-    }
-    function mul(uint a, uint b) internal pure returns (uint c) {
-        c = a * b;
-        require(a == 0 || c / a == b);
-    }
-    function div(uint a, uint b) internal pure returns (uint c) {
-        require(b > 0);
-        c = a / b;
-    }
-}
-
-
-// ----------------------------------------------------------------------------
-// ERC Token Standard #20 Interface
-// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
-// ----------------------------------------------------------------------------
-contract ERC20Interface {
-    function totalSupply() public constant returns (uint);
-    function balanceOf(address tokenOwner) public constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
-    function transfer(address to, uint tokens) public returns (bool success);
-    function approve(address spender, uint tokens) public returns (bool success);
-    function transferFrom(address from, address to, uint tokens) public returns (bool success);
-
-    event Transfer(address indexed from, address indexed to, uint tokens);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
-}
-
-
-// ----------------------------------------------------------------------------
-// Contract function to receive approval and execute function in one call
-//
-// Borrowed from MiniMeToken
-// ----------------------------------------------------------------------------
-contract ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
-}
-
-
-// ----------------------------------------------------------------------------
-// Owned contract
-// ----------------------------------------------------------------------------
-contract Owned {
-    address public owner;
-    address public newOwner;
-
-    event OwnershipTransferred(address indexed _from, address indexed _to);
-
-    constructor() public {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner {
-        require(msg.sender == owner);
-        _;
-    }
-
-    function transferOwnership(address _newOwner) public onlyOwner {
-        newOwner = _newOwner;
-    }
-    function acceptOwnership() public {
-        require(msg.sender == newOwner);
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-        newOwner = address(0);
-    }
-}
-
 
 // ----------------------------------------------------------------------------
 // ERC20 Token, with the addition of symbol, name and decimals and a
 // fixed supply
 // ----------------------------------------------------------------------------
-contract FixedSupplyToken is ERC20Interface, Owned {
+contract FixedSupplyToken is ERC20Interface, Owned, ApproveAndCallFallBack {
     using SafeMath for uint;
 
     string public symbol;
     string public name;
     uint8 public decimals;
-    uint totalSupply;
+    uint supply;
 
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowed;
@@ -116,13 +26,13 @@ contract FixedSupplyToken is ERC20Interface, Owned {
     // ------------------------------------------------------------------------
     // Constructor
     // ------------------------------------------------------------------------
-    constructor(string _symbol, string _name, uint _totalSupply) public {
+    constructor(string _symbol, string _name, uint _supply) public {
         symbol = _symbol;
         name = _name;
         decimals = 18;
-        totalSupply = _totalSupply * 10**uint(decimals);
-        balances[owner] = _totalSupply;
-        emit Transfer(address(0), owner, _totalSupply);
+        supply = _supply * 10**uint(decimals);
+        balances[owner] = _supply;
+        emit Transfer(address(0), owner, _supply);
     }
 
 
@@ -130,7 +40,7 @@ contract FixedSupplyToken is ERC20Interface, Owned {
     // Total supply
     // ------------------------------------------------------------------------
     function totalSupply() public view returns (uint) {
-        return totalSupply.sub(balances[address(0)]);
+        return supply.sub(balances[address(0)]);
     }
 
 
