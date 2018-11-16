59d3b1df1e78881a0dc233ee8102057f005c4c52
Jason Kiraly <jaykiraly@gmail.com>
2018-11-16 11:20:49 -0800

    Added scaffolding to compile/deploy. Both are *incomplete* and broken with this contract. Will fix after I code the contract and need to deploy on ropsten.

---
+++ compile.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const solc = require("solc");
+const fs = require("fs-extra");
+
+const buildPath = path.resolve(__dirname, "build");
+
+// Remove build folder.
+fs.removeSync(buildPath);
+
+// const leaderboardPath = path.resolve(__dirname, "contracts", "leaderboard", "Leaderboard.sol");
+// const safeMathPath = path.resolve(__dirname, "contracts", "leaderboard", "SafeMath.sol");
+// const stringUtilsPath = path.resolve(__dirname, "contracts", "leaderboard", "StringUtils.sol");
+// const reentrancyGuardPath = path.resolve(__dirname, "contracts", "leaderboard", "ReentrancyGuard.sol");
+
+// const input = {
+//   sources: {
+//     "Leaderboard.sol": fs.readFileSync(leaderboardPath, "utf8"),
+//     "SafeMath.sol": fs.readFileSync(safeMathPath, "utf8"),
+//     "StringUtils.sol": fs.readFileSync(stringUtilsPath, "utf8"),
+//     "ReentrancyGuard.sol": fs.readFileSync(reentrancyGuardPath, "utf8")
+//   }
+// }
+
+// const output = solc.compile(input, 1);
+// // console.log('o', output, output.contracts);
+// const contracts = output.contracts;
+// fs.ensureDirSync(buildPath);
+
+// for (let contract in contracts) {
+//   const filename = contract.split(".")[0];
+//   console.log(`interface for ${contract}: `, contracts[contract].interface)
+//   fs.outputJsonSync(
+//     path.resolve(buildPath, `${filename}.json`),
+//     contracts[contract]
+//   );
+// }
+
+console.log('compile successful!');
+console.log('Your ABIs might need to be updated on any frontend apps.')
