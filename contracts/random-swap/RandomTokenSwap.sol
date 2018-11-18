bab6c85315f3d94603393ef6b08315a33db900cd
Jason Kiraly <jaykiraly@gmail.com>
2018-11-18 14:01:06 -0800

    More improvements. Approve then transfer works. Now to get basic random working.

--- contracts/random-swap/RandomTokenSwap.sol
+++ contracts/random-swap/RandomTokenSwap.sol
@@ -1,12 +1,8 @@
 pragma solidity ^0.4.24;
 
 
-// Partially psuedo code. Will fix later. From Remix.
-contract RandomTokenSwap {
-    BasicToken oneToken = BasicToken(0xb17C1feBB85CDBEb99b00da0BeefB091AA5f8F9d);
-    BasicToken twoToken = BasicToken(0x4872232D95D5d524eEa13844Aa0a745BBEA850Eb);
-    BasicToken threeToken = BasicToken(0x816FF8390E41594caB5F1d1A0d0F4d24Cd824909);
-    
+// Partially psuedo code. Will fix later. From Remix. Need to add in other contracts to get this deploying right.
+contract RandomTokenSwap is Owned {
     mapping (bytes32 => address) public tokenAddresses;
     
     constructor() public {
@@ -15,7 +11,7 @@ contract RandomTokenSwap {
         tokenAddresses[convertStringToKey("THREE")] = 0x816FF8390E41594caB5F1d1A0d0F4d24Cd824909;
     }
     
-    function checkAllowance(string _tokenName) public view returns(uint){
+    function checkAllowance(string _tokenName) public view returns(uint) {
         BasicToken token = BasicToken(tokenAddresses[convertStringToKey(_tokenName)]);
         address fromUser = msg.sender;
 
@@ -23,23 +19,31 @@ contract RandomTokenSwap {
     }
     
     
-    function sendTransactionIfApproved() public {
+    function sendTransactionIfApproved(string _tokenName) public {
         address fromUser = msg.sender;
-        uint decimals = oneToken.decimals();
+        BasicToken token = BasicToken(tokenAddresses[convertStringToKey(_tokenName)]);
+        uint decimals = token.decimals();
         uint singleToken = 1 * 10 ** uint(decimals);
         // Adding in default users from my MetaMask for meow
-        oneToken.transferFrom(fromUser, address(this), singleToken);
-    }
-    
-    function transferFromContract(address _address) public {
-        uint decimals = oneToken.decimals();
-        uint singleToken = 1 * 10 ** uint(decimals);
-        oneToken.transfer(_address, singleToken);
+        token.transferFrom(fromUser, address(this), singleToken);
+        transferFromContract(singleToken, token);
     }
     
     function getBalanceOfToken(address _tokenAddress) public view returns(uint) {
         BasicToken token = BasicToken(_tokenAddress);
         return token.balanceOf(address(this));
+    }
+    
+    function addTokenToSwap(string _tokenName, address _tokenAddress) public onlyOwner {
+        tokenAddresses[convertStringToKey(_tokenName)] = _tokenAddress;
+    }
+    
+    function checkTokenAddress(string _tokenName) public view returns(address) {
+        return tokenAddresses[convertStringToKey(_tokenName)];
+    }
+    
+    function transferFromContract(uint _singleToken, BasicToken _token) private {
+        _token.transfer(msg.sender, _singleToken);
     }
     
     function convertStringToKey(string key) private pure returns (bytes32 ret) {
